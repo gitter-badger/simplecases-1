62385d00595441e3953decf68a3faaaf36f14f40
Claudio Guerra <claudevandort@gmail.com>
2014-10-19 21:51:27 -0300

    remove webapp project and update dockerfiles

--- docker.sh
+++ docker.sh
@@ -1,5 +1,12 @@
-docker build -t rails docker/rails
-docker build -t pg docker/pg
-#sudo docker rm -f rails; sudo docker rm -f pg
-sudo docker run -d -p 127.0.0.1:80:80 --name rails -v .:/var/www:ro rails
-sudo docker run -d -p 127.0.0.1:5432:5432 --name pg pg
+#sudo echo "127.0.0.1 webapp.dev" >> /etc/hosts
+ln webapp/Gemfile .docker/rails/Gemfile
+ln webapp/Gemfile.lock .docker/rails/Gemfile.lock
+docker build -t rails .docker/rails
+docker build -t pg .docker/pg
+rm .docker/rails/Gemfile .docker/rails/Gemfile.lock
+docker rm -f rails; docker rm -f pg
+docker run -d -p 127.0.0.1:5432:5432 --name pg pg
+docker run -d --net="host" -v ~/Github/simplecases/webapp:/var/www/webapp:ro --name rails rails
+#docker run -d -p 127.0.0.1:80:80 --name rails rails
+#-v $PWD/webapp:/var/www:ro rails
+#sudo docker run --name rails -i -t rails /bin/bash
