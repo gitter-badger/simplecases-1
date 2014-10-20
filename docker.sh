b3c100cb7197f6295b43236bd0697168aa2e2ca5
Claudio Guerra <claudevandort@gmail.com>
2014-10-19 23:03:49 -0300

    update dockerfiles

--- docker.sh
+++ docker.sh
@@ -1,12 +1,12 @@
 #sudo echo "127.0.0.1 webapp.dev" >> /etc/hosts
-ln webapp/Gemfile .docker/rails/Gemfile
-ln webapp/Gemfile.lock .docker/rails/Gemfile.lock
-docker build -t rails .docker/rails
+ln Gemfile .docker/rails/Gemfile
+ln Gemfile.lock .docker/rails/Gemfile.lock
 docker build -t pg .docker/pg
+docker build -t rails .docker/rails
 rm .docker/rails/Gemfile .docker/rails/Gemfile.lock
 docker rm -f rails; docker rm -f pg
 docker run -d -p 127.0.0.1:5432:5432 --name pg pg
-docker run -d --net="host" -v ~/Github/simplecases/webapp:/var/www/webapp:ro --name rails rails
+docker run -d --net="host" -v ~/Github/simplecases:/var/www/webapp:ro --name rails rails
 #docker run -d -p 127.0.0.1:80:80 --name rails rails
 #-v $PWD/webapp:/var/www:ro rails
 #sudo docker run --name rails -i -t rails /bin/bash
