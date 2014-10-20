5aed377e7e79222677ed2cf14407cdd41bb46b91
Claudio Guerra <claudevandort@gmail.com>
2014-10-20 10:26:45 -0300

    update dockerfiles and db connection file

--- docker.sh
+++ docker.sh
@@ -6,7 +6,7 @@ docker build -t rails .docker/rails
 rm .docker/rails/Gemfile .docker/rails/Gemfile.lock
 docker rm -f rails; docker rm -f pg
 docker run -d -p 127.0.0.1:5432:5432 --name pg pg
-docker run -d --net="host" -v ~/Github/simplecases:/var/www/webapp:ro --name rails rails
+docker run -d --net="host" -v $PWD:/var/www/webapp:ro --name rails rails
 #docker run -d -p 127.0.0.1:80:80 --name rails rails
 #-v $PWD/webapp:/var/www:ro rails
 #sudo docker run --name rails -i -t rails /bin/bash
