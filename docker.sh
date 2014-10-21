0e0ea0b87baca6edb0d4c24ce99d8a20f699d7b0
Claudio Guerra <claudevandort@gmail.com>
2014-10-21 10:06:09 -0300

    update dockerfiles

--- docker.sh
+++ docker.sh
@@ -1,8 +1,9 @@
 #sudo echo "127.0.0.1 webapp.dev" >> /etc/hosts
 ln Gemfile .docker/rails/Gemfile
 ln Gemfile.lock .docker/rails/Gemfile.lock
-docker build -t pg .docker/pg
-docker build -t rails .docker/rails
+docker build -t simplecases-dev-pg .docker/pg
+docker build -t simplecases-dev-rails rails .docker/rails/development
+docker build -t simplecases-prod-rails rails .docker/rails/production
 rm .docker/rails/Gemfile .docker/rails/Gemfile.lock
 docker rm -f rails; docker rm -f pg
 docker run -d -p 127.0.0.1:5432:5432 --name pg pg
